2225e3636308545f009fa378ab51a4eb06457ee7
lpoulin <lucienpoulin@live.com>
2020-08-17 18:17:19 -0400

    added vuetify

--- src/main.js
+++ src/main.js
@@ -1,29 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuetify from 'vuetify/lib'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
 
-
-
-Vue.use(vuetify)
-
-const vuetify = new Vuetify({
-  theme: {
-    themes: {
-      dark: {
-      primary: '#607d8b',
-      secondary: '#673ab7',
-      accent: '#2196f3',
-      error: '#f44336',
-      warning: '#ff9800',
-      info: '#009688',
-      success: '#4caf50'
-      }
-    },
-  },
-})

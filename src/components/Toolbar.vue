f49f1c6b0af0eded13472ba3322bcc23132c0d69
lpoulin <lucienpoulin@live.com>
2020-08-17 19:38:50 -0400

    updated toolbar

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app-bar
+      app
+      color="primary"
+      dark
+      height="120px"
+    >
+    <v-container fluid>
+        <v-toolbar-title>Emily Tomasello</v-toolbar-title>
+    </v-container>
+    <v-spacer></v-spacer>
+        <v-btn text class="nav-link">Home</v-btn>
+        <v-btn text class="nav-link">Photography</v-btn>
+    
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'Toolbar'
+}
+</script>
+
+<style scoped>
+    .header-container {
+    }
+    .nav-link {
+        color: #fff;
+    }
+</style>
